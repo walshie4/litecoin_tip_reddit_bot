1528d122f43eab8830295bca0978e761876f68e5
Adam Walsh <adamwalsh14@gmail.com>
2017-09-10 17:25:12 -0700

    Add some functions to wallet.js

--- wallet.js
+++ wallet.js
@@ -1,5 +1,7 @@
 const litecoin = require('node-litecoin');
 
+const MIN_CONFIRMATIONS = 6;
+
 const ltc = new litecoin({
   host: 'localhost',
   port: 8832,
@@ -7,4 +9,34 @@ const ltc = new litecoin({
   pass: process.env.LTC_PASS,
 });
 
+function getAddressForUser(user) {
+  return ltc.getNewAddress(user);
+}
 
+function getBalanceForAddress(address) {
+  return ltc.getReceivedByAddress(address);
+}
+
+function sendCoinsFromTo(sendToAddress, sendFromAddress, sendFromUser, amount) {
+  const memo = `Sending ${amount} from ${sendFromUser} ${sendFromAddress} to ${sendToAddress}`;
+  return ltc.sendFrom(sendFromUser, sendToAddress, amount, MIN_CONFIRMATIONS, memo);
+}
+
+function lockWallet() {
+  return ltc.walletLock();
+}
+
+function unlockWallet() {
+  const pass = process.env.LOCK_PASSPHRASE;
+  return ltc.walletPassPhrase(pass);
+}
+
+
+
+module.exports = {
+  getAddressForUser,
+  getBalanceForAddress,
+  sendCoinsFromTo,
+  lockWallet,
+  unlockWallet,
+};
